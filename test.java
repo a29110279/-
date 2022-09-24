1e59fc1429d702188c323ef42364f293673428fa
WainLin <wainctie1219@gmial.com>
2022-09-25 02:55:34 +0800

    123

--- test.java
+++ test.java
@@ -1,1 +1,17 @@
-//123
+import java.awt.Frame;
+import java.awt.event.*;
+
+public class test{
+    public static void main(String[] args){
+        Frame win = new Frame("Hello Mother Fucker!");
+        win.setLocation(200, 300);
+        win.setSize(500, 300);
+        win.setVisible(true);
+        win.addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent e){
+                System.exit(1);
+            }
+        });
+
+    }
+}
