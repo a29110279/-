f8c4833e9eb2ef7282fc1d170b08490ab688d077
a29110279 <c29110279@gmail.com>
2022-09-25 02:52:41 +0800

    new file

--- test.java
+++ test.java
@@ -1,1 +1,11 @@
-//123
+import java.awt.Frame;
+import java.awt.event.*;
+public class test {
+    public static void main(String[] args) {
+		Frame win = new Frame("Hello");
+        win.setLocation(200,300);
+        win.setSize(500,300);
+        win.setVisible(true);
+        
+	}
+}
