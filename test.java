7438606e7bfeefab24d93b43201503b4b3af6795
a29110279 <c29110279@gmail.com>
2022-09-26 16:19:17 +0800

    1

--- test.java
+++ test.java
@@ -3,7 +3,7 @@ import java.awt.event.*;
 
 public class test{
     public static void main(String[] args){
-        Frame win = new Frame("Hello Mother Fucker!");
+        Frame win = new Frame("Hello BOD!");
         win.setLocation(200, 300);
         win.setSize(500, 300);
         win.setVisible(true);
@@ -14,9 +14,5 @@ public class test{
         });
 
     }
-<<<<<<< HEAD
 }
->>>>>>> 7c416fe87621e98a524cd895a567161e22bf9900
-=======
-}
->>>>>>> 3c4305e69d06ecdb42140c89b3c14d85ba010720
+
