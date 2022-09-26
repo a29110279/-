cf0cf073431a8f02710af3b329030682c8789729
a29110279 <c29110279@gmail.com>
2022-09-26 22:04:45 +0800

    50%

--- test.java
+++ test.java
@@ -1,6 +1,12 @@
 import java.awt.Frame;
 import java.awt.event.*;
 
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.*;
+
 public class test{
     public static void main(String[] args){
         Frame win = new Frame("Hello BOD!");
@@ -14,5 +20,19 @@ public class test{
         });
 
     }
+
+    public static void showImage(){
+        JFrame frame = new JFrame("place_test");
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel();
+        label.setIcon(new ImageIcon("img/300.jfif"));
+        panel.add(label);
+        frame.add(panel);
+        frame.setBounds(200, 300, 500, 300);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
 }
 
